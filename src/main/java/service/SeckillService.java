8ebd32e95559fff9b09a51c8e911ebdf4c5100eb
heqianqian <330423640@qq.com>
2017-02-28 23:43:33 +0800

    创建业务层类和实现类

---
+++ src/main/java/service/SeckillService.java
@@ -0,0 +1,8 @@
+package service;
+
+/**
+ * 秒杀业务层类
+ * Created by heqianqian on 2017/2/28.
+ */
+public interface SeckillService {
+}
---
+++ src/main/java/service/impl/SeckillServiceImpl.java
@@ -0,0 +1,10 @@
+package service.impl;
+
+import service.SeckillService;
+
+/**
+ * 秒杀业务层实现类
+ * Created by heqianqian on 2017/2/28.
+ */
+public class SeckillServiceImpl implements SeckillService{
+}
